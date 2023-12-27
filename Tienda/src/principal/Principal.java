8625bc868207c4bbceb93a2e3df6004addcb178a
xwhiterr <aquinojonathan37@gmail.com>
2023-12-27 17:53:27 -0300

    Practica09

---
+++ Tienda/src/principal/Principal.java
@@ -0,0 +1,17 @@
+//@author Walter Aquino
+
+package principal;
+
+import tienda.Login;
+
+public class Principal {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Login ventana1 = new Login();        
+    }
+    
+}
